d736f6bc2c3c2178aac3964fd94745c10f754162
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-09-12 18:06:17 +0530

    Addin programs

---
+++ Basic_Program/Add.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Program
+{
+    public class add
+    {
+       
+        public int Add()
+        {
+            
+            Console.WriteLine("please enter the Number1");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Number2");
+            int b = Convert.ToInt32(Console.ReadLine());
+            return a + b;
+        }
+
+        public int Sub()
+        {
+            Console.WriteLine("please enter the Number1");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Number2");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            return a - b;
+        }
+        public int Mul()
+        {
+            Console.WriteLine("please enter the Number1");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Number2");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            return a * b;
+        }
+        public double Div()
+        {
+            Console.WriteLine("please enter the Number1");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Number2");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            return a / b;
+        }
+    }
+}
---
+++ Basic_Program/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Basic_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 for add");
+            Console.WriteLine("Enter 2 for sub");
+            Console.WriteLine("Enter 3 for mul");
+            Console.WriteLine("Enter 4 for div");
+            int n = int.Parse(Console.ReadLine());
+            switch(n)
+            {
+                case 1:
+                    add _add = new add();
+                    
+                    int res = _add.Add();
+                    Console.WriteLine(res);
+                    
+                    
+                    break;
+                case 2:
+                    add _sub = new add();
+
+                    int res1 = _sub.Sub();
+                    Console.WriteLine(res1);
+
+                    break;
+                case 3:
+                    add _mul = new add();
+
+                    int res2 = _mul.Mul();
+                    Console.WriteLine(res2);
+
+                    break;
+                case 4:
+                    add _div = new add();
+
+                    double res3 = _div.Div();
+                    Console.WriteLine(res3);
+
+                    break;
+                    default:
+                    Console.WriteLine("failed");
+                    break ;
+            }
+            
+           
+        }
+    }
+}
